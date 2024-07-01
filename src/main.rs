e625230894def2e31b8b41bf3b50561b35b9d0e3
Arqam Zia <i221170@nu.edu.pk>
2024-07-01 22:57:59 +0500

    Listening?

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
-use std::net::TcpListener;
+use std::io::Read;
 use std::io::Write;
+use std::net::TcpListener;
+use std::net::TcpStream;
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let get = b"GET / HTTP/1.1\r\n";
+    let response = if buffer.starts_with(get) {
+        "HTTP/1.1 200 OK\r\n\r\n"
+    } else {
+        "HTTP/1.1 404 Not Found\r\n\r\n"
+    };
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
 
 fn main() {
 
