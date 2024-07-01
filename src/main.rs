0d365e8bc06c070aa7c17f9b592c257d611d4fc0
Arqam Zia <i221170@nu.edu.pk>
2024-07-01 23:46:02 +0500

    Read header

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,67 @@
 use std::{
-    io::{BufRead, BufReader, Read, Write},
+    io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
+    str,
 };
-fn handle_path(first_line: &String) -> String {
-    let path_list = first_line.split(" ").collect::<Vec<&str>>();
-    println!("path_list {:?}", path_list);
-    if path_list[1] == "/" {
-        return "HTTP/1.1 200 OK\r\n\r\n".to_owned();
-    }
-    if !path_list[1].starts_with("/echo") {
-        return "HTTP/1.1 404 Not Found\r\n\r\n".to_owned();
-    }
-    let len = path_list.len();
-    let echo_sub_path = path_list[1].split("/").collect::<Vec<&str>>()[len - 1];
-    println!("sub path {:?}", echo_sub_path);
-    let path_string_len = echo_sub_path.len();
-    return format!(
-        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-        path_string_len, echo_sub_path
-    );
-}
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let _http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-    let first_line = &_http_request[0];
-    let path = first_line.split(" ").collect::<Vec<&str>>()[1];
-    let response = if path == "/" {
-        "HTTP/1.1 200 OK\r\n\r\n"
-    } else {
-        "HTTP/1.1 404 Not Found\r\n\r\n"
-    };
-    let response = handle_path(&_http_request[0]);
-    if let Ok(_) = stream.write_all(response.as_bytes()) {
-        println!("accepted new connection");
-    } else {
-        eprintln!("Error occured!");
-    }
-}
+static RESPONSE_OK: &str = "200 OK";
+static RESPONSE_NOT_FOUND: &str = "404 Not Found";
 fn main() {
-    println!("Logs from your program will appear here!");
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => handle_connection(stream),
+            Ok(stream) => handle_req(stream),
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
 }
+fn handle_req(mut stream: TcpStream) {
+    let lines = parse_request(&stream);
+    let req_details: Vec<_> = lines[0].split(" ").collect();
+    let headers = &lines[1..];
+    let res: String;
+    if req_details[1].starts_with("/echo") {
+        let input: Vec<_> = req_details[1]
+            .split("/")
+            .filter(|s| !s.is_empty())
+            .collect();
+        println!("input: {:?}", input);
+        res = echo_response(input[1]);
+    } else if req_details[1].starts_with("/user-agent") {
+        let found = headers
+            .iter()
+            .find(|header| header.starts_with("User-Agent:"));
+        match found {
+            Some(header) => {
+                let user_agent: Vec<_> = header.split(" ").collect();
+                res = echo_response(user_agent[1]);
+            }
+            None => res = format!("HTTP/1.1 {}\r\n\r\n", RESPONSE_NOT_FOUND),
+        }
+    } else {
+        match req_details[1] {
+            "/" => res = format!("HTTP/1.1 {}\r\n\r\n", RESPONSE_OK),
+            _ => res = format!("HTTP/1.1 {}\r\n\r\n", RESPONSE_NOT_FOUND),
+        }
+    }
+    stream.write_all(res.as_bytes()).unwrap()
+}
+fn parse_request(mut stream: &TcpStream) -> Vec<String> {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_req: Vec<_> = buf_reader
+        .lines()
+        .map(|res| res.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    http_req
+}
+fn echo_response(input: &str) -> String {
+    format!(
+        "HTTP/1.1 {}\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+        RESPONSE_OK,
+        input.len(),
+        input
+    )
+}
+
