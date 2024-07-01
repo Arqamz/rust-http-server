7c74baa1a26b4ab76e330f5be37abd89e7b2d926
Arqam Zia <i221170@nu.edu.pk>
2024-07-01 23:19:01 +0500

    Adding body length in response

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,53 @@
-use std::io::Read;
-use std::io::Write;
-use std::net::TcpListener;
-use std::net::TcpStream;
+use std::{
+    io::{BufRead, BufReader, Read, Write},
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
+fn handle_path(first_line: &String) -> String {
+    let path_list = first_line.split(" ").collect::<Vec<&str>>();
+    println!("path_list {:?}", path_list);
+    if path_list[1] == "/" {
+        return "HTTP/1.1 200 OK\r\n\r\n".to_owned();
+    }
+    if !path_list[1].starts_with("/echo") {
+        return "HTTP/1.1 404 Not Found\r\n\r\n".to_owned();
+    }
+    let len = path_list.len();
+    let echo_sub_path = path_list[1].split("/").collect::<Vec<&str>>()[len - 1];
+    println!("sub path {:?}", echo_sub_path);
+    let path_string_len = echo_sub_path.len();
+    return format!(
+        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+        path_string_len, echo_sub_path
+    );
+}
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
-    stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
-    let get = b"GET / HTTP/1.1\r\n";
-    let response = if buffer.starts_with(get) {
+    let buf_reader = BufReader::new(&mut stream);
+    let _http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    let first_line = &_http_request[0];
+    let path = first_line.split(" ").collect::<Vec<&str>>()[1];
+    let response = if path == "/" {
         "HTTP/1.1 200 OK\r\n\r\n"
     } else {
         "HTTP/1.1 404 Not Found\r\n\r\n"
     };
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    let response = handle_path(&_http_request[0]);
+    if let Ok(_) = stream.write_all(response.as_bytes()) {
+        println!("accepted new connection");
+    } else {
+        eprintln!("Error occured!");
+    }
 }
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-                handle_connection(_stream);
-            }
+            Ok(stream) => handle_connection(stream),
             Err(e) => {
                 println!("error: {}", e);
             }
