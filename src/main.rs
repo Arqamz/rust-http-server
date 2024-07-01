890f211def6d58c05dc6421b55061e2b395ef668
Arqam Zia <i221170@nu.edu.pk>
2024-07-01 23:07:57 +0500

    Listening?

--- src/main.rs
+++ src/main.rs
@@ -16,19 +16,15 @@ fn handle_connection(mut stream: TcpStream) {
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
-
 fn main() {
-
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
-
-    
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => {
+            Ok(_stream) => {
                 println!("accepted new connection");
-                _stream.write(b"HTTP/1.1 200 OK\r\n\r\n").expect("200 \n");
+                handle_connection(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
